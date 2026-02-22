c500832f40d92919c6afeb5aea0c24506679f210
Vinoth-KR <vkrmailbox@gmail.com>
2026-02-22 12:23:54 +0530

    while-loops

--- basic-sandbox/sandbox.py
+++ basic-sandbox/sandbox.py
@@ -193,3 +193,11 @@ users = {
     },
 }
 
+# removing all instances of a particular item in the list
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit','cat']
+print(pets)
+
+while 'cat' in pets:
+    pets.remove('cat')
+
+print(pets)
