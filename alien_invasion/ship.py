42a61f639e2d6a41186418ddf330a03e27f26061
Vinoth-KR <vkrmailbox@gmail.com>
2026-03-12 23:26:09 +0530

    alienInvasion : Adding key events

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -7,14 +7,28 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
-        # load the hip image and get its rect
+        # load the ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
         # start each new ship at the bottom center
         self.rect.midbottom = self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
+        self.moving_right = False
+        self.moving_left = False
+    
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
