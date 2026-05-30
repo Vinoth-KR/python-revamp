b7b8e3499edefcea1d1d43e1ec2b34d3f89487d4
Vinoth-KR <vkrmailbox@gmail.com>
2026-05-31 00:33:58 +0530

    python-repos from github visualization

--- api-interaction/python_repos.py
+++ api-interaction/python_repos.py
@@ -1,4 +1,5 @@
 import requests
+import plotly.express as px
 
 url = 'https://api.github.com/search/repositories'
 url += '?q=language:python&sort:stars+stars:>10000'
@@ -14,7 +15,26 @@ print(f'Complete results: {not response_dict["incomplete_results"]}')
 repo_dicts = response_dict['items']
 print(f'Number of repositories returned: {len(repo_dicts)}')
 
-repo = repo_dicts[0]
-print(f'\nKeys: {len(repo_dicts[0])}')
-for key in sorted(repo.keys()):
-    print(key)
+repo_names, stars, hover_texts = [], [], []
+for repo_dict in repo_dicts:
+    # print(f'Name: {repo_dict["name"]}')
+    # print(f'Owner: {repo_dict["owner"]["login"]}')
+    # print(f'Stars: {repo_dict["stargazers_count"]}')
+    # print(f'Repository: {repo_dict["html_url"]}')
+    # print(f'Description: {repo_dict["description"]}')
+    repo_names.append(repo_dict["name"])
+    stars.append(repo_dict["stargazers_count"])
+
+    owner = repo_dict["owner"]["login"]
+    description = repo_dict["description"]
+    hover_text = f'{owner}<br>{description}'
+    hover_texts.append(hover_text)
+
+
+#Visualization
+title = 'Most-Starred Python Projects on GitHub'
+labels = {'x': 'Repository', 'y': 'Stars'}
+fig = px.bar(x=repo_names, y=stars, title=title, labels=labels, hover_name=hover_texts)
+
+fig.update_layout(title_font_size=28, xaxis_title_font_size=18, yaxis_title_font_size=18)
+fig.show()
